1a8520724366417e0cdbb6e5809b08a7b47d4cdd
Jason <thebizztech@me.com>
2011-10-07 09:52:18 -0500

    Fixed bug where POST did not work correctly.  Now when posting just send an array of data with post($data)

--- Curl.php
+++ Curl.php
@@ -11,11 +11,21 @@ class Curl {
 	
 	var $url = "";
 	var $headers = array(); //Headers are built in set_headers() and passed in execute()
+	var $post_data = "";
+	var $fields_string = "";
 	
 	//set_url() must be set by Codeigniter controller or models
 	public function set_url($url)
 	{
 		$this->url = $url;
+		return $this;
+	}
+
+	public function build_post_string()
+	{
+		$this->fields_string = null;
+		foreach($this->post_data as $key=>$value) { $this->fields_string .= $key.'='.$value.'&'; }
+		$this->fields_string = rtrim($this->fields_string,"&");
 		return $this;
 	}
 	
@@ -24,30 +34,33 @@ class Curl {
 	{
 		$this->headers = array(
 						CURLOPT_URL => $this->url,
-						CURLOPT_HEADER => FALSE,
-						CURLOPT_CONNECTTIMEOUT => 30,
-						CURLOPT_FOLLOWLOCATION => TRUE,
+						CURLOPT_VERBOSE => 1,
+						CURLOPT_SSL_VERIFYPEER => FALSE,
+						CURLOPT_TIMEOUT => 30,
 						CURLOPT_RETURNTRANSFER => TRUE
 		);
 
 		if($type == 'post')
 		{
 			$this->headers[CURLOPT_POST] = TRUE;
+			$this->build_post_string();
+			$this->headers[CURLOPT_POSTFIELDS] = $this->fields_string;
 		}
 		return $this;
 	}
 
 	//Set the headers and process curl via a GET
-    public function get()
-    {
+	public function get()
+	{
 		return $this->set_headers()->execute();
-    }
+	}
 	
 	//Set the headers and process curl via a POST
-	public function post()
-    {
+	public function post($data)
+	{
+	    $this->post_data = $data;
 		return $this->set_headers('post')->execute();
-    }
+	}
 	
 	//Starts curl and sets headers and returns the data in a string
 	private function execute()
@@ -57,9 +70,8 @@ class Curl {
 		curl_setopt_array($ch, $this->headers);
 		// grab URL
 		$result = curl_exec($ch);
-		
+	
 		curl_close($ch);
-		
 		return $result;
 	}
 }
